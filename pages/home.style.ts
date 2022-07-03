a762b43a9b328fcfc1cbd87937e8020a7c0f3ae5
Emmanuel Okp <okpunorrex@gmail.com>
2022-07-03 22:28:44 +0100

    removed home.style.ts

--- pages/home.style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Grid = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 300px;
-	grid-gap: 1em;
-`;
-export const Column = styled.div``;
